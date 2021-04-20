3428fe27ae028035f7850c23b639dd25fd361323
Jason Dunn <dunnjm814@gmail.com>
2021-04-20 15:51:57 -0700

    some style

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+  const [videoData, setVideoData] = useState('')
+const fileTypes = ["video/*"];
+function validFileType(file) {
+  return fileTypes.includes(file.type);
+}
+  const onSubmit = () => {
+
+  }
   return (
     <div className="App">
+      <h1>Video to mp3 converter</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="file-input" className='custom-file-input'>
+          <input id="file-input" type="file" onChange={(e) => {
 
+          }} accept="video/*"></input>
+        </label>
+      </form>
     </div>
   );
 }
