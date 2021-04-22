7dc232adc5475b6145e443add045b8e60705b3d0
Jason Dunn <dunnjm814@gmail.com>
2021-04-21 17:04:44 -0700

    more descriptive names.

--- back/api/converter.py
+++ back/api/converter.py
@@ -1,22 +1,27 @@
+import os
 from flask import Blueprint, jsonify, request
 import imageio
 from moviepy.editor import *
+from moviepy.audio import *
 
-# imageio moviepy.editor
 
 
 convert = Blueprint('convert', __name__)
 
 
 @convert.route('', methods=['POST'])
-def covert_mp4():
-  file = request.files['mp3']
+def convert_mp4():
+  file = request.files['mp3'].filename
   print('hey its a file', file)
   if file:
-    video = VideoFileClip(file)
-    audio = vid.audio
+    print('hey its a file again', file)
+    video_clip = VideoFileClip(file)
+    print('hey its the VideoFileClip', video_clip)
+    audio_clip = video_clip.audio
+    converted = audio_clip.write_audiofile("converted.mp3")
 
-    converted = audio.write_audiofile("converted.mp3")
+    video_clip.close()
+    audio_clip.close()
 
     return  jsonify(converted)
   else:
