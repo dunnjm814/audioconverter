fcdb35bd67c1230d8463e4bbd9d5aa23cf615440
Jason Dunn <dunnjm814@gmail.com>
2021-06-23 08:11:05 -0700

    added utility file for video filetype list

---
+++ react-app/src/utils/mimetype.js
@@ -0,0 +1,13 @@
+export const fileTypes = [
+  "video/mp4",
+  "video/quicktime",
+  "video/x-mpegURL",
+  "video/x-flv",
+  "video/MP2T",
+  "video/3gpp",
+  "video/3gp2",
+  "video/x-msvideo",
+  "video/x-ms-wmv",
+  "video/ogg",
+  "video/webm",
+]
